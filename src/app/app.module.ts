d9dc8937a933b87904cf5b4023e24ca929e73f86
Sara Briki <sarabriki@insat.u-carthage.tn>
2023-01-18 00:37:31 +0100

    added home page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,21 @@ import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppComponent } from './app.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import {AppComponent} from "./app.component";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {FormsModule, ReactiveFormsModule} from '@angular/forms'
-import {MatIconModule} from "@angular/material/icon";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCardModule } from '@angular/material/card';
-import {MatSelectModule} from "@angular/material/select";
-import {HttpClientModule} from "@angular/common/http";
-import {MatNativeDateModule} from "@angular/material/core";
-import {MaterialExampleModule} from "../material.module";
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MaterialExampleModule } from '../material.module';
+import { RightPanelComponent } from './components/right-panel/right-panel.component';
+import { LeftPanelComponent } from './components/left-panel/left-panel.component';
+import { DailyTasksListComponent } from './components/daily-tasks-list/daily-tasks-list.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignupComponent, HomeComponent],
+  declarations: [AppComponent, LoginComponent, SignupComponent, HomeComponent, RightPanelComponent, LeftPanelComponent, DailyTasksListComponent],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
--- src/app/components/daily-tasks-list/daily-tasks-list.component.ts
+++ src/app/components/daily-tasks-list/daily-tasks-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from './models/Task';
+import { Task } from '../../models/Task';
 
 @Component({
   selector: 'daily-tasks-list',
