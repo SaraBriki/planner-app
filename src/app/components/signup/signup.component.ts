51f8981d88be08a13b7586de8b515b1e0ab01059
Bechir-Brahem <bachbrahem@gmail.com>
2023-01-18 01:31:44 +0100

    add singup

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  constructor(private cd: ChangeDetectorRef) {
+  }
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  confirmPassword = new FormControl('', [Validators.required]);
+
+  passwordsMatch:boolean=true
+  checkPasswords(el1:string,el2:string){
+    this.passwordsMatch=el1!==el2
+  }
+
 
 }
