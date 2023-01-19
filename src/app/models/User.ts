d343b2b1e4120493b9e520bef226ff09356c3ef0
Bechir-Brahem <bachbrahem@gmail.com>
2023-01-19 16:27:27 +0100

    final

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,6 +1,6 @@
 export class User {
-  password: string | null ;
-  username: string |null;
+  password: string | null;
+  username: string | null;
 
   constructor(username: string | null, password: string | null) {
     this.password = password;
